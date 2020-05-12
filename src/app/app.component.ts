00d2aeba3b1e33ab751a00b0eed11448e6edfaf1
Lawrence Liu <Lawrencehudson23@gmail.com>
2020-05-11 18:36:12 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'assignment3-directives';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+    return this.log;
+  }
 }
